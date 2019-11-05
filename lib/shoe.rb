f43afb1527311fc57d39048ddf6dd01cbafc94ab
Ruel Tingling <rvtingling@gmail.com>
2019-11-05 01:19:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,22 +2,18 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-BRAND = []
+  BRANDS = []
 
-def initialize(brand)
+  def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand)
+    else
+      BRANDS << brand
+    end
   end
 
-
- def brand=(brand)
-   @brand = brand
-   BRAND << brand
- end 
- 
-  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-   
-end
+end 
